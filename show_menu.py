a1b7acc7e6ac63743c21e77a93ee5a4d7097cce3
Maral <m.kafi.s@gmail.com>
2021-12-10 15:11:52 +0330

    first version

---
+++ show_menu.py
@@ -0,0 +1,6 @@
+# show menu
+print("1- Enter 1 to give two integers!")
+print("2- Enter 2 to add two given integers!")
+print("3- Enter 3 to minus two given integers!")
+print("4- Enter 4 to multiply two given integers!")
+print("5- Enter 5 to divide two given integers!")
